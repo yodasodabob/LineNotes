54b83e5964477017a55a30b9d3d77fc403dc1209
Laurence Ruberl <bknd225@gmail.com>
2017-06-03 14:10:46 -0400

    Add code for authorizing and adding notes

--- line-notes4/src/App.js
+++ line-notes4/src/App.js
@@ -8,74 +8,90 @@ import LineForm from './LineForm'
 // import { PublicRoute, PrivateRoute } from './RouteHelpers'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
+  state = {
       uid: null,
-      notes: [],
-    }
+      notes: {},
   }
 
-  // componentWillMount() {
-  //   this.getUserFromLocalStorage()
-  //   base.onAuth((user) => {
-  //     if (user) {
-  //       this.authHandler(null, { user });
-  //     } else {
-  //       this.signOut()
-  //     }
-  //   })
-  // }
-
-  // getUserFromLocalStorage = () => {
-  //   const uid = localStorage.getItem('uid')
-  //   if (!uid) return
-  //   this.setState({ uid })
-  // }
-
-  // authHandler = (err, authData) => {
-  //     if (err) {
-  //       console.error(err);
-  //       return;
-  //     }
-  //     this.setState({
-  //       uid: authData.user.uid,
-  //     })
-  //     localStorage.setItem('uid', authData.user.id)
-  //     this.setupNotes()
-  //   }
-
-  //   authed = () => {
-  //     return !!this.state.uid
-  //   }
-
-  //   signOut = () => {
-  //     base.unauth()
-  //     this.setState({ uid: null })
-  //     localStorage.removeItem('uid')
-  //   }
-
-  setUpNotes() {
-    this.ref = base.syncState(
-      `${this.state.currentPlay}/notes`,
-      {
-        context: this,
-        state: 'actors'
+  componentWillMount() {
+    auth.onAuthStateChanged(
+      (user) => {
+        if (user) {
+          this.authHandler({ user: user})
+        }
       }
     )
   }
 
-  setCurrentPlay = (showName) => {
-    this.currentPlay = showName
+  setUpNotes() {
+    this.ref = base.syncState(
+      `${this.state.uid}/notes`,
+      {
+        context: this,
+        state: 'notes'
+      }
+    )
   }
+
+authHandler = (authData) => {
+    this.setState(
+      { uid: authData.user.uid },
+      this.setUpNotes())
+  }
+
+  // setCurrentPlay = (showName) => {
+  //   this.currentPlay = showName
+  // }
 
   lineNote() {
     return{
-      pageNum: 0,
+      id: `note-${Date.now}`,
+      show: '',
+      pageNum: '',
       issue: '',
       fullLine: '',
     }
+  }
 
+  addNote = () => {
+    const notes = {...this.state.notes}
+    const note = this.lineNote()
+    notes[note.id] = note
+    this.setState({ notes })
+    
+  }
+
+  saveNote = (note) => {
+    const notes = {...this.state.notes}
+    notes[note.id] = note
+    this.setState({ notes })
+  }
+
+  signOut = () => {
+    const updatedState = {
+      uid: null, 
+      notes: {}
+    }
+    this.setState({ uid: null, notes: {updatedState} })
+    auth.signOut()
+  }
+
+renderThings() {
+    const actions = {
+      saveNote: this.saveNote,
+      // removeNote: this.removeNote,       //not yet implemented
+    }
+
+    return(
+      <div>
+        <SignOut signOut={this.signOut} />
+        <AddThingButton addThing={this.addThing} />
+        <ThingList
+          things={this.state.things}
+          {...actions}
+          />
+      </div>
+    )
   }
 
   render() {
