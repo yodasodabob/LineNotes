75e1bfc4df88724dc9f4876aaf5035a338fa0818
Laurence Ruberl <bknd225@gmail.com>
2017-06-22 16:19:57 -0400

    Fix new user form

--- src/NewUserForm.js
+++ src/NewUserForm.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
+import './NewUserForm.css'
 
 class NewUserForm extends Component {
 
     render() {
         return(
-            <div>
+            <div className="newUserForm">
                 <h2>You are a new user; please fill out this form </h2>
                 <form onSubmit={this.props.formHandler}>
-                    <input type="text" name='firstName' />
-                    <input type="text" name='lastName' />
-                    <input type="text" name='role' />
-                    <button type='submit' className='button success radius'>Submit</button>
+                    <input type="text" name='firstName' placeholder='First name' className='formField' />
+                    <input type="text" name='lastName' placeholder='Last name' className='formField' />
+                    <input type="text" name='role' placeholder='Role in Production (Actor, ASM, etc)' className='formField' />
+                    <button type='submit' className='button success extended'>Submit</button>
                 </form>
             </div>
         )
