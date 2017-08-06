bbba724a91f708d38859d4b0ed8d663de055b824
Laurence Ruberl <bknd225@gmail.com>
2017-08-06 17:28:04 -0400

    Add date sorting and bind both dropdowns to same form; edit changeNotesToDisplay accordingly

--- src/ActorPanel.js
+++ src/ActorPanel.js
@@ -18,40 +18,46 @@ class ActorPanel extends Component {
         return (optionsObj)
     }
 
-    changeNotes = (ev, optparam) => {
+    changeNotes = (ev, optParam1, optParam2) => {
         ev.preventDefault()
         const form = ev.target
         const options = {
-            param: optparam,
-            query: form.date.value
+            param1: optParam1,
+            param2: optParam2,
+            query1: form[optParam1].value,
+            query2: form[optParam2].value,
         }
         this.props.changeNotesToDisplay(options)
     }
 
     render() {
+        const sortNotes = (a, b) => {
+            let fa = a.split('-').join('')
+            let fb = b.split('-').join('')
+            console.log(fa.match(/\d+/)[0] - fb.match(/\d+/)[0])
+            return fb.match(/\d+/)[0] - fa.match(/\d+/)[0]
+        }
         let dateArray = this.generateOptions(this.props.notes, 'date')
         let showArray = this.generateOptions(this.props.notes, "show")
         return(
             <div className="actorPanel column medium-2">
-                <form className="changeDrop" onSubmit={(ev) => {this.changeNotes(ev, 'date')}}>
+                <form className="changeDrop" onSubmit={(ev) => {this.changeNotes(ev, 'date', 'show')}}>
                     <select name="date" id="date">
                         {
                             Object
                             .keys(dateArray)
+                            .sort(sortNotes)
                             .map(dateNum => <option id={dateArray[dateNum]} key={dateNum}>{dateArray[dateNum]}</option>)
                         }
                     </select>
-                    <button type="submit" className="button success">Change date</button>
-                </form>
-                <form className="changeDrop" onSubmit={(ev) => {this.changeNotes(ev, 'show')}}>
-                    <select name="show" id="date">
+                    <select name="show" id="show">
                         {
                             Object
                             .keys(showArray)
                             .map(showname => <option id={showArray[showname]} key={showname}>{showArray[showname]}</option>)
                         }
                     </select>
-                    <button type='submit' className='button success'>Change Show</button>
+                    <button type="submit" className="button success">Filter Notes</button>
                 </form>
             </div>
         )
