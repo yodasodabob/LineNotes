10889a441c5a708fd6f32c5d7cb6cb072923e787
Laurence Ruberl <bknd225@gmail.com>
2017-09-06 23:08:08 -0400

    Add even more debugging

--- src/ActorPanel.js
+++ src/ActorPanel.js
@@ -12,16 +12,16 @@ class ActorPanel extends Component {
     componentWillMount() {
         const filter = {...this.state.filter}
         filter.show = this.getShowFromLocalStorage()
-        console.log(filter.show)
+        console.log("filter.show:" + filter.show)
         if (!filter.show) {
             const showOptions = this.generateOptions(this.props.notes, 'show')
-            console.log(showOptions)
+            console.log("showOptions:" + showOptions)
             const showOptionsKeys = Object.keys(showOptions)
-            console.log(showOptionsKeys)
+            console.log("showOptionsKeys:" + showOptionsKeys)
             filter.show = showOptions[showOptionsKeys[0]]
             localStorage.setItem('show', filter.show)
         }
-        console.log(filter.show)
+        console.log("filter.show:" + filter.show)
         const dates = this.generateOptions(this.props.notes, 'date', filter.show)
         const sortNotes = (a, b) => {
             let fa = a.split('-').join('')
@@ -29,9 +29,9 @@ class ActorPanel extends Component {
             return fb.match(/\d+/)[0] - fa.match(/\d+/)[0]
         }
         const dateKeysSort = Object.keys(dates).sort(sortNotes)
-        console.log(dateKeysSort)
+        console.log("filter.show:" + dateKeysSort)
         filter.date = dates[dateKeysSort[0]]
-        console.log(filter)
+        console.log("filter:" + filter)
         this.setState({ filter })
         this.changeNotes('date', 'show', filter.date, filter.show)
     }
