e6f7f6b6d5433b269cf897cb42046a7aeb975b26
Laurence Ruberl <bknd225@gmail.com>
2017-09-10 15:27:42 -0400

    Add Constructor and some methods for Script Interpreter

--- linenotes3js/src/ScriptTools.js
+++ linenotes3js/src/ScriptTools.js
@@ -1,5 +1,17 @@
+// Script interpreter class with methods to interpret a script that's formatted to the specifications of linenotes3
+
 class ScriptInterpreter {
+    // Takes a list containing the lines of the script, and an object containing 
+    constructor(script) {
+        this.script = script
+    }
+
+    setCastList(castList) {
+        this.castList = castList
+    }
+
     // Takes script as a list of lines and returns a list of objects with scene title and line number
+
     generateScenes(script) {
         let scenes = {}
         for (i = 0; i < script.length; i++){
@@ -13,7 +25,34 @@ class ScriptInterpreter {
         return scenes
     }
 
+    // Returns the speaker(s) saying a given line
+    // WIP don't use yet;
+    multiSpeakerCheck(line){
+        let splitLine = line.split(" ")
+        if (splitLine.length > 1) {
+            if (splitLine[1] == "&") {
+                return([splitLine[0], splitLine[2].split(".")]) 
+            } else if (splitLine[0].charAt(splitLine[0].length - 1)) {
+                return([splitLine.join("").split(",")])
+            } else {
+                return splitLine.split(".")
+            }
+        }
+    }
+
+    /* Looks for the closest speaker and returns it;
     
+    */
+    lookForSpeaker(currentLine) {
+        let speaker = null
+        while (!speaker) {
+            let line = currentLine
+            if (this.script[line].charAt(0) === "#") {
+                speaker = this.script[line].split("#").split(".")[0]
+                return speaker
+            }
+        }
+    }
 }
 
 export default ScriptInterpreter
