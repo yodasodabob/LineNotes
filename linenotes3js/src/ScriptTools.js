ddc46fdcc6343de7c5b9c458328aa9d398fc00e0
Laurence Ruberl <bknd225@gmail.com>
2017-09-12 19:30:52 -0400

    Store scenelist in the object

--- linenotes3js/src/ScriptTools.js
+++ linenotes3js/src/ScriptTools.js
@@ -3,7 +3,7 @@
 class ScriptInterpreter {
     // Takes a list containing the lines of the script, and an object containing 
     constructor(script) {
-        this.script = script
+        this.lines = script
     }
 
     setCastList(castList) {
@@ -12,25 +12,30 @@ class ScriptInterpreter {
 
     // Takes script as a list of lines and returns a list of objects with scene title and line number
 
-    generateScenes(script) {
-        let scenes = {}
-        for (i = 0; i < script.length; i++){
-            if (script[i].charAt(0) == "$") {
-                scenes[i] = {
-                    sceneName: script[i].split("$")[0],
-                    sceneLine: i,
+    generateScenes() {
+        if (!this.sceneList) {
+            let scenes = {}
+            for (let i = 0; i < this.lines.length; i++){
+                if (this.lines[i].charAt(0) === "$") {
+                    scenes[i] = {
+                        sceneName: this.lines[i].split("$")[0],
+                        sceneLine: i,
+                    }
                 }
             }
+            this.sceneList = scenes
+            return scenes
+        } else {
+            return this.sceneList
         }
-        return scenes
     }
 
     // Returns the speaker(s) saying a given line
-    // WIP don't use yet;
+    // WIP don't use yet; functionality built into lookForSpeak, returns only the first speaker
     multiSpeakerCheck(line){
         let splitLine = line.split(" ")
         if (splitLine.length > 1) {
-            if (splitLine[1] == "&") {
+            if (splitLine[1] === "&") {
                 return([splitLine[0], splitLine[2].split(".")]) 
             } else if (splitLine[0].charAt(splitLine[0].length - 1)) {
                 return([splitLine.join("").split(",")])
@@ -47,10 +52,11 @@ class ScriptInterpreter {
         let speaker = null
         while (!speaker) {
             let line = currentLine
-            if (this.script[line].charAt(0) === "#") {
-                speaker = this.script[line].split("#").split(".")[0]
+            if (this.lines[line].charAt(0) === "#") {
+                speaker = this.lines[line].split("#").split(".")[0]
                 return speaker
             }
+            line -= 1
         }
     }
 }
