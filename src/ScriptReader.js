fafdb51e511aa264a5fbdf684df2324129be29df
Laurence Ruberl <bknd225@gmail.com>
2017-09-26 12:32:36 -0400

    Persist lines in localstorage and allow them to be removed (ln3jscmd command @exit)

--- src/ScriptReader.js
+++ src/ScriptReader.js
@@ -6,12 +6,27 @@ class ScriptReader extends Component {
         lines: null,
     }
 
+    componentWillMount() {
+        if (!this.state.lines) {
+            const lines = JSON.parse(localStorage.getItem("lines"))
+            if (lines) {this.setState({ lines })}
+        }
+    }
+
+    removeLines() {
+        let lines = {...this.state.lines}
+        lines = null
+        localStorage.removeItem("lines")
+        this.setState({ lines })
+    }
+
     readFile = (ev) => {
         let reader = new FileReader();
         reader.onload = () => {
             let text = reader.result;
             let lines = text.split('\n');
             this.setState({ lines })
+            localStorage.setItem("lines", JSON.stringify(lines))
         };
         reader.readAsText(ev.target.files[0])
     }
@@ -21,7 +36,7 @@ class ScriptReader extends Component {
         return(
             <div>
                 {this.state.lines ? 
-                    <LinesDisplayer getCurrentInfo={this.props.getCurrentInfo} changeDate={this.props.changeDate} addNote={this.props.addNote} lines={this.state.lines} /> : 
+                    <LinesDisplayer removeLines={this.removeLines.bind(this)} getCurrentInfo={this.props.getCurrentInfo} changeDate={this.props.changeDate} addNote={this.props.addNote} lines={this.state.lines} /> : 
                     <input type="file" onChange={this.readFile} accept=".txt, .doc, .rtf, .docx"/>
                 }
             </div>
