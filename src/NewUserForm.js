3f87db6381e46e6323ea2382c0ccbb4ae6951e77
Laurence Ruberl <bknd225@gmail.com>
2017-10-12 12:04:37 -0400

    Change role select to dropdown

--- src/NewUserForm.js
+++ src/NewUserForm.js
@@ -10,6 +10,12 @@ class NewUserForm extends Component {
                 <form onSubmit={this.props.formHandler}>
                     <input type="text" name='firstName' placeholder='First name' className='formField' />
                     <input type="text" name='lastName' placeholder='Last name' className='formField' />
+                    <select name='role'>
+                        <option value="Actor">Actor</option>
+                        <option value="ASM">Assistant Stage Manager</option>
+                        <option value="SM">Stage Manager</option>
+                        <option value="PA">Production Assistant</option>
+                    </select>
                     <input type="text" name='role' placeholder='Role in Production (Actor, ASM, etc)' className='formField' />
                     <button type='submit' className='button success extended'>Submit</button>
                 </form>
